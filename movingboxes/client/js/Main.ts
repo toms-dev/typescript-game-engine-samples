ac9bce1c9538568a30f670ae8c9e69f321f29c01
Tom GUILLERMIN <tom.guillermin@gmail.com>
2016-01-21 10:06:29 +0100

    Looks like it's fixed?

--- movingboxes/client/js/Main.ts
+++ movingboxes/client/js/Main.ts
@@ -1,12 +1,9 @@
 
-/// <reference path="../node_modules/typescript-game-engine-client/typings.d.ts" />
-
 import {Game} from 'typescript-game-engine-client';
 
 import My2DRenderer from "./game/My2DRenderer";
 import MyTextRenderer from "./game/MyTextRenderer";
 import ColorButton from "./game/ColorButton";
-
 
 var game = new Game();
 game.start();
