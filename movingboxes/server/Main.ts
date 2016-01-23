f0022c8e4652e980a85c9b459b8dc6899201a22f
Tom GUILLERMIN <tom.guillermin@gmail.com>
2016-01-23 23:31:02 +0100

    Updated the way the project is loaded.

---
+++ movingboxes/server/Main.ts
@@ -0,0 +1,5 @@
+
+import {Main} from "typescript-game-engine-server";
+
+Main(__dirname+"/maps/MainMap");
+//Main(__dirname+"/maps/EmptyMap");
---
+++ movingboxes/server/maps/EmptyMap.ts
@@ -0,0 +1,10 @@
+
+import {Declare, Map} from "typescript-game-engine-server";
+
+@Declare.Map
+export default class EmptyMap extends Map {
+
+	setup():void {
+	}
+
+}
