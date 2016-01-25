802e8be37cc8879956b274d1382ddfd3151826db
Tom GUILLERMIN <tom.guillermin@gmail.com>
2016-01-25 22:09:03 +0100

    Added webpack build.

---
+++ movingboxes/client/webpack.config.js
@@ -0,0 +1,47 @@
+var path = require('path');
+
+module.exports = {
+	//entry: './js/Main2.js',
+	entry: './js/game/Main3.js',
+	output: {
+		filename: 'bundle.js',
+		path: 'build'
+	},
+	devtool: 'inline-source-map',
+	resolve: {
+		// Add `.ts` and `.tsx` as a resolvable extension.
+		extensions: ['', '.webpack.js', '.web.js', '.js', '.ts', '.tsx']
+	},
+	resolveLoader: {
+		root: [path.join(__dirname, 'node_modules')]
+	},
+	module: {
+		loaders: [
+			// all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+			//{ test: /\.tsx?$/, loader: 'babel-loader!ts-loader' },
+			{ test: /\.js$/, loader: 'babel-loader' }
+		]
+	}
+};
+
+/*module.exports = {
+	entry: './js/Main.ts',
+	resolve: {
+		//extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+		extensions: ['', '.ts']
+	},
+	output: {
+		path: path.join(__dirname, 'build'),
+		filename: 'bundle.js'
+	},
+	devtool: 'inline-source-map',
+	module: {
+		loaders: [
+			{ test: /\.ts$/, loader: 'ts-loader' },
+			{
+				test: path.join(__dirname, 'src'),
+				loader: 'babel-loader'
+			}
+		]
+	}
+};*/
