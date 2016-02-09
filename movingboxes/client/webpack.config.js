125879fe65b92be98c164728fe9a356ddb5b5ad4
Tom GUILLERMIN <tom.guillermin@gmail.com>
2016-02-10 00:23:20 +0100

    Upgraded build process.

--- movingboxes/client/webpack.config.js
+++ movingboxes/client/webpack.config.js
@@ -8,10 +8,17 @@ module.exports = {
 	},
 	devtool: 'inline-source-map',
 	resolve: {
-		root: path.join(__dirname, "js"),
-		// Add `.ts` and `.tsx` as a resolvable extension.
-		//extensions: ['', '.webpack.js', '.web.js', '.js', '.ts', '.tsx']
-		extensions: ['', '.js', '.ts', '.tsx']
+		//root: path.join(__dirname, "js"),
+		root: path.join(__dirname, "node_modules"),
+
+		// We need blank space support because some module are explicitly referenced with their extension
+		// We need .js support because of external libraries.
+		// We need .js.map support because WebStorm automatically compile files and
+		extensions: ['', '.ts', '.js' , '.js.map']
+
+		// Additional note
+		// This is the extensions to be used on a clean distribution of the typescript-game-engine-client
+		// extensions: ['','.ts', '.js'],
 	},
 	resolveLoader: {
 		root: [path.join(__dirname, 'node_modules')]
@@ -19,8 +26,10 @@ module.exports = {
 	module: {
 		loaders: [
 			// all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-			{ test: /\.ts$/, loader: 'babel-loader!ts-loader' }
-			//{ test: /\.js$/, loader: 'babel-loader' }
+			{ test: /\.ts$/, loader: 'ts-loader' },
+
+			// Handle externally generated sourcemaps. Not needed if compiled with a clean copy of the client engine.
+			{ test: /\.js.map$/, loader: 'json-loader' }
 		]
 	}
 };
